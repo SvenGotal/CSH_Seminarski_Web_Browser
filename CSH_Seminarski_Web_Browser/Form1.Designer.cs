bb239fdb52f02b757c7604b4c65939a80328284f
esvegot <esvegot@HR-00004162>
2020-02-08 13:11:48 +0100

    Implement Go Back button

--- CSH_Seminarski_Web_Browser/Form1.Designer.cs
+++ CSH_Seminarski_Web_Browser/Form1.Designer.cs
@@ -33,7 +33,19 @@
             this.labelURL = new System.Windows.Forms.Label();
             this.buttonGO = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonFavoritesAdd = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.favoritesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser
@@ -41,7 +53,7 @@
             this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.webBrowser.Location = new System.Drawing.Point(12, 85);
+            this.webBrowser.Location = new System.Drawing.Point(12, 122);
             this.webBrowser.Margin = new System.Windows.Forms.Padding(10);
             this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser.Name = "webBrowser";
@@ -50,25 +62,25 @@
             // 
             // textBoxURL
             // 
-            this.textBoxURL.Location = new System.Drawing.Point(67, 16);
+            this.textBoxURL.Location = new System.Drawing.Point(259, 21);
             this.textBoxURL.Name = "textBoxURL";
-            this.textBoxURL.Size = new System.Drawing.Size(792, 26);
+            this.textBoxURL.Size = new System.Drawing.Size(600, 26);
             this.textBoxURL.TabIndex = 1;
-            this.textBoxURL.Enter += new System.EventHandler(this.textBoxURL_Enter);
             this.textBoxURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxURL_KeyDown);
             // 
             // labelURL
             // 
             this.labelURL.AutoSize = true;
-            this.labelURL.Location = new System.Drawing.Point(15, 16);
+            this.labelURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelURL.Location = new System.Drawing.Point(197, 22);
             this.labelURL.Name = "labelURL";
-            this.labelURL.Size = new System.Drawing.Size(46, 20);
+            this.labelURL.Size = new System.Drawing.Size(56, 25);
             this.labelURL.TabIndex = 2;
             this.labelURL.Text = "URL:";
             // 
             // buttonGO
             // 
-            this.buttonGO.Location = new System.Drawing.Point(876, 12);
+            this.buttonGO.Location = new System.Drawing.Point(876, 17);
             this.buttonGO.Name = "buttonGO";
             this.buttonGO.Size = new System.Drawing.Size(156, 35);
             this.buttonGO.TabIndex = 3;
@@ -81,13 +93,103 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.buttonBack);
+            this.panel1.Controls.Add(this.buttonFavoritesAdd);
             this.panel1.Controls.Add(this.buttonGO);
             this.panel1.Controls.Add(this.labelURL);
             this.panel1.Controls.Add(this.textBoxURL);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(12, 37);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1208, 72);
             this.panel1.TabIndex = 4;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(21, 21);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(131, 31);
+            this.buttonBack.TabIndex = 5;
+            this.buttonBack.Text = "Go Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonFavoritesAdd
+            // 
+            this.buttonFavoritesAdd.Location = new System.Drawing.Point(1038, 17);
+            this.buttonFavoritesAdd.Name = "buttonFavoritesAdd";
+            this.buttonFavoritesAdd.Size = new System.Drawing.Size(156, 35);
+            this.buttonFavoritesAdd.TabIndex = 4;
+            this.buttonFavoritesAdd.Text = "Add to favorites";
+            this.buttonFavoritesAdd.UseVisualStyleBackColor = true;
+            this.buttonFavoritesAdd.Click += new System.EventHandler(this.buttonFavoritesAdd_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1239, 33);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProfileToolStripMenuItem,
+            this.selectProfileToolStripMenuItem,
+            this.favoritesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newProfileToolStripMenuItem
+            // 
+            this.newProfileToolStripMenuItem.Name = "newProfileToolStripMenuItem";
+            this.newProfileToolStripMenuItem.Size = new System.Drawing.Size(216, 34);
+            this.newProfileToolStripMenuItem.Text = "&New profile";
+            // 
+            // selectProfileToolStripMenuItem
+            // 
+            this.selectProfileToolStripMenuItem.Name = "selectProfileToolStripMenuItem";
+            this.selectProfileToolStripMenuItem.Size = new System.Drawing.Size(216, 34);
+            this.selectProfileToolStripMenuItem.Text = "&Select profile";
+            // 
+            // favoritesToolStripMenuItem
+            // 
+            this.favoritesToolStripMenuItem.Name = "favoritesToolStripMenuItem";
+            this.favoritesToolStripMenuItem.Size = new System.Drawing.Size(216, 34);
+            this.favoritesToolStripMenuItem.Text = "&Favorites";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 34);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.versionToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(172, 34);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(172, 34);
+            this.versionToolStripMenuItem.Text = "&Version";
             // 
             // Form1
             // 
@@ -96,11 +198,16 @@
             this.ClientSize = new System.Drawing.Size(1239, 612);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "CSH Web Browser";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,6 +218,17 @@
         private System.Windows.Forms.Label labelURL;
         private System.Windows.Forms.Button buttonGO;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectProfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem favoritesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button buttonFavoritesAdd;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.Button buttonBack;
     }
 }
 
--- CSH_Seminarski_Web_Browser/Form1.cs
+++ CSH_Seminarski_Web_Browser/Form1.cs
@@ -13,18 +13,26 @@ namespace CSH_Seminarski_Web_Browser
 {
     public partial class Form1 : Form
     {
-        private string placeholder = "http://";
-        private string placeholderSecure = "https://";
+        private string placeholder; 
+        private List<string> history;
+        private int historyIndex;
 
         public Form1()
         {
             InitializeComponent();
+
+            placeholder = "http://";
+            history = new List<string>();
+            historyIndex = -1;
 
             textBoxURL.Text = placeholder;
             textBoxURL.GotFocus += TextBoxURL_GotFocus;
             textBoxURL.LostFocus += TextBoxURL_LostFocus;
 
         }
+
+        /*********************************************************************************************/
+        /* Event handlers */
 
         private void TextBoxURL_LostFocus(object sender, EventArgs e)
         {
@@ -33,7 +41,6 @@ namespace CSH_Seminarski_Web_Browser
                 textBoxURL.Text = placeholder;
             }
         }
-
         private void TextBoxURL_GotFocus(object sender, EventArgs e)
         {
             if( textBoxURL.Text == placeholder)
@@ -42,27 +49,58 @@ namespace CSH_Seminarski_Web_Browser
             }
         }
 
+
         private void buttonGO_Click(object sender, EventArgs e)
         {
             Navigate();
         }
+        private void textBoxURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(validateKey(e, Keys.Enter) && !string.IsNullOrWhiteSpace(textBoxURL.Text))
+            {
+                Navigate();
+            }
+        }
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
 
+
+        private void buttonFavoritesAdd_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+
+        /*********************************************************************************************/
+        /* Helper methods */
+
+        /// <summary>
+        /// Checks if the key entered equals the desired key.
+        /// </summary>
+        /// <param name="key">Input key</param>
+        /// <param name="chosenKey">Desired key</param>
+        /// <returns>True if key equals desired key.</returns>
+        private bool validateKey(KeyEventArgs key, Keys chosenKey)
+        {
+            return key.KeyCode == chosenKey;
+        }
+
+        /// <summary>
+        /// Navigates the webBrowser control to the desired URL
+        /// </summary>
         private void Navigate()
         {
             webBrowser.Navigate(textBoxURL.Text);
         }
 
-        private void textBoxURL_KeyDown(object sender, KeyEventArgs e)
-        {
-            if(e.KeyCode == Keys.Enter && !string.IsNullOrWhiteSpace(textBoxURL.Text))
-            {
-                Navigate();
-            }
-        }
-
-        private void textBoxURL_Enter(object sender, EventArgs e)
-        {
-
-        }
     }
 }
