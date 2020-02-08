4308fc032c9de2efcec1b58d2f0924e07ada8e38
esvegot <esvegot@HR-00004162>
2020-02-08 10:23:46 +0100

    Add web browser control, add URL textbox, put placeholder in URL textbox

--- CSH_Seminarski_Web_Browser/Form1.Designer.cs
+++ CSH_Seminarski_Web_Browser/Form1.Designer.cs
@@ -28,20 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.labelURL = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 78);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1430, 522);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(113, 12);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(792, 26);
+            this.textBoxURL.TabIndex = 1;
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.Location = new System.Drawing.Point(49, 18);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(46, 20);
+            this.labelURL.TabIndex = 2;
+            this.labelURL.Text = "URL:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1454, 612);
+            this.Controls.Add(this.labelURL);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.webBrowser);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label labelURL;
     }
 }
 
--- CSH_Seminarski_Web_Browser/Form1.cs
+++ CSH_Seminarski_Web_Browser/Form1.cs
@@ -12,11 +12,32 @@ namespace CSH_Seminarski_Web_Browser
 {
     public partial class Form1 : Form
     {
+        private string placeholder = "http://...";
+
         public Form1()
         {
             InitializeComponent();
+
+            textBoxURL.Text = placeholder;
+            textBoxURL.GotFocus += TextBoxURL_GotFocus;
+            textBoxURL.LostFocus += TextBoxURL_LostFocus;
+
         }
 
+        private void TextBoxURL_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxURL.Text))
+            {
+                textBoxURL.Text = placeholder;
+            }
+        }
 
+        private void TextBoxURL_GotFocus(object sender, EventArgs e)
+        {
+            if( textBoxURL.Text == placeholder)
+            {
+                textBoxURL.Text = "";
+            }
+        }
     }
 }
