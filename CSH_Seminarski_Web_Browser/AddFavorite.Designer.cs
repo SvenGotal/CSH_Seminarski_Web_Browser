2805c14313fe90ea25cf798fc5b5a0145085cf41
esvegot <esvegot@HR-00004162>
2020-02-09 19:39:32 +0100

    Implement AddFavorite form and functionality

---
+++ CSH_Seminarski_Web_Browser/AddFavorite.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CSH_Seminarski_Web_Browser
+{
+    partial class AddFavorite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.labelUrl = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(68, 175);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(117, 38);
+            this.buttonCancel.TabIndex = 0;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(312, 175);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(117, 38);
+            this.buttonOk.TabIndex = 1;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.Location = new System.Drawing.Point(109, 27);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.Size = new System.Drawing.Size(50, 20);
+            this.labelUrl.TabIndex = 2;
+            this.labelUrl.Text = "URL: ";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(20, 83);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(139, 20);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Name/Description:";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(180, 24);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(287, 26);
+            this.textBoxUrl.TabIndex = 4;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(180, 80);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(287, 26);
+            this.textBoxDescription.TabIndex = 5;
+            // 
+            // AddFavorite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 244);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelUrl);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonCancel);
+            this.Name = "AddFavorite";
+            this.Text = "AddFavorite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Label labelUrl;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.TextBox textBoxDescription;
+    }
+}
---
+++ CSH_Seminarski_Web_Browser/AddFavorite.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSH_Seminarski_Web_Browser
+{
+    public partial class AddFavorite : Form
+    {
+        public AddFavorite()
+        {
+            InitializeComponent();
+        }
+
+        private Form1 mainForm;
+        private string placeholder;
+        public AddFavorite(Form mainForm)
+        {
+            this.mainForm = mainForm as Form1;
+            InitializeComponent();
+
+            try
+            {
+                placeholder = this.mainForm.webBrowser.Url.ToString();
+                textBoxUrl.Text = placeholder;
+                textBoxUrl.GotFocus += TextBoxUrl_GotFocus;
+                textBoxUrl.LostFocus += TextBoxUrl_LostFocus;
+
+            }
+            catch (Exception ex)
+            {
+                placeholder = "";
+            }
+
+
+        }
+
+        private void TextBoxUrl_LostFocus(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(textBoxUrl.Text))
+                textBoxUrl.Text = placeholder;
+        }
+
+        private void TextBoxUrl_GotFocus(object sender, EventArgs e)
+        {
+            textBoxUrl.Text = "";
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string filename = this.mainForm.CurrentUser.Name + "_favorites.xml";
+                string url = textBoxUrl.Text;
+                string name = textBoxDescription.Text;
+
+                if (!string.IsNullOrWhiteSpace(url) && !string.IsNullOrWhiteSpace(name))
+                {
+                    Favorite fav = new Favorite(url, name);
+                    Persistence.WriteFavorite(filename, fav);
+
+                    this.mainForm.UpdateCurrentUserFavorites();
+
+                    this.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("No white spaces allowed!", "Error!", MessageBoxButtons.OK);
+                    return;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
